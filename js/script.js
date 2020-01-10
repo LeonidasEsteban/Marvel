70cb69f19bad48150f8e2351e3f10451f768f7f3
Sami A.Delgado <samiadelgado@gmail.com>
2020-01-11 00:08:06 +0100

    carousel añadido y estilos basicos
    Queda ajustar algunos estilos del carousel para que se vea mas parecido al de marvel

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ const mobileOpen = ()=>{
 const menu = document.getElementById('mobile-menu');
 menu.addEventListener("click", mobileOpen);
 
+/*
+Codigo jquery carousel
+*/
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+  });
+
